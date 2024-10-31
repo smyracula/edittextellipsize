b8c3c69f23bb9d753fc9295b70f24a3ae719d68c
Osman Korcan Andaç <osman.korcan@gmail.com>
2024-10-31 17:29:41 +0300

    Fix Hint Problem

--- app/src/main/java/com/smyracula/edittextellipsize/MainActivity.kt
+++ app/src/main/java/com/smyracula/edittextellipsize/MainActivity.kt
@@ -9,17 +9,21 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -31,9 +35,12 @@ import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -60,6 +67,7 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(
     modifier: Modifier = Modifier.padding(16.dp),
@@ -67,16 +75,17 @@ fun MainScreen(
 ) {
 
     val focusRequester = remember { FocusRequester() }
-    var textFieldValue by remember { mutableStateOf(TextFieldValue(viewModel.text.value)) }
     var isFocused by remember { mutableStateOf(false) }
-
+    var placeHolder = "Buraya text gelecek"
     Column(modifier = modifier) {
 
-        if (isFocused) {
+        if (isFocused || (!isFocused && viewModel.text.value.isEmpty())) {
             BasicTextField(
-                value = textFieldValue,
+                value = viewModel.text.value,
                 maxLines = 1,
-                onValueChange = { viewModel.onTextChanged(textFieldValue.text) },
+                onValueChange = { newText ->
+                    viewModel.onTextChanged(newText)
+                },
                 textStyle = TextStyle(fontSize = 18.sp, color = Color.Black),
                 modifier = Modifier
                     .border(1.dp, Color.Gray)
@@ -85,7 +94,33 @@ fun MainScreen(
                     .focusRequester(focusRequester)
                     .onFocusChanged { focusState ->
                         isFocused = focusState.isFocused
-                    }
+                    },
+                decorationBox = @Composable { innerTextField ->
+                    TextFieldDefaults.TextFieldDecorationBox(
+                        value = viewModel.text.value,
+                        visualTransformation = VisualTransformation.None,
+                        innerTextField = innerTextField,
+                        placeholder = placeHolder.let {
+                            {
+                                Text(
+                                    modifier = Modifier.semantics {
+                                        contentDescription = placeHolder.lowercase()
+                                    },
+                                    text = placeHolder
+                                )
+                            }
+                        },
+                        singleLine = true,
+                        enabled = true,
+                        interactionSource = MutableInteractionSource(),
+                        contentPadding = PaddingValues(
+                            start = 12.dp,
+                            top = 22.dp,
+                            end = 12.dp
+                        )
+                    )
+                }
+
             )
             LaunchedEffect(Unit) {
                 focusRequester.requestFocus()
@@ -107,7 +142,6 @@ fun MainScreen(
             )
         }
 
-        //Added for changing focus
         Spacer(modifier = Modifier.height(10.dp))
         BasicTextField(
             value = viewModel.text2.value,
